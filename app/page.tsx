7b27e5f9e9a047d4f4fed88ca7324040ea3651d0
Anna Dowling <info@keytonutrition.com>
2026-06-10 14:28:34 +0100

    Fix import paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 // app/page.tsx
-import HomeNav from "@/components/home/HomeNav";
-import HomeHero from "@/components/home/HomeHero";
-import HomeBand from "@/components/home/HomeBand";
-import HomeWhy from "@/components/home/HomeWhy";
-import HomeFeatures from "@/components/home/HomeFeatures";
-import HomeDarkFeatures from "@/components/home/HomeDarkFeatures";
-import HomeFounder from "@/components/home/HomeFounder";
-import HomePricing from "@/components/home/HomePricing";
-import HomeFAQ from "@/components/home/HomeFAQ";
-import HomeCTA from "@/components/home/HomeCTA";
-import HomeFooter from "@/components/home/HomeFooter";
+import HomeNav from "@/app/components/home/HomeNav";
+import HomeHero from "@/app/components/home/HomeHero";
+import HomeBand from "@/app/components/home/HomeBand";
+import HomeWhy from "@/app/components/home/HomeWhy";
+import HomeFeatures from "@/app/components/home/HomeFeatures";
+import HomeDarkFeatures from "@/app/components/home/HomeDarkFeatures";
+import HomeFounder from "@/app/components/home/HomeFounder";
+import HomePricing from "@/app/components/home/HomePricing";
+import HomeFAQ from "@/app/components/home/HomeFAQ";
+import HomeCTA from "@/app/components/home/HomeCTA";
+import HomeFooter from "@/app/components/home/HomeFooter";
 import "../app/home.css";
 
 export default function HomePage() {

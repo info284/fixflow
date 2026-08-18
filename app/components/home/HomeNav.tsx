e79831dd93c00386f4b3f89b2b33994e45cfdb1e
Anna Dowling <info@keytonutrition.com>
2026-08-18 22:17:27 +0100

    Update FixFlow website styling and comparisons

--- app/components/home/HomeNav.tsx
+++ app/components/home/HomeNav.tsx
@@ -1,23 +1,26 @@
 // app/components/home/HomeNav.tsx
 
+import Link from "next/link";
 
 export default function HomeNav() {
   return (
     <nav className="homeNav">
       <div className="homeNavInner">
-        <div className="homeNavLogo">
+
+        <Link href="/" className="homeNavLogo">
           Fix<span>Flow</span>
-        </div>
-<div className="homeNavActions">
-  <a className="homeNavLogin" href="/login">
-    Log in
-  </a>
+        </Link>
 
-  <a className="homeNavCta" href="/signup">
-    Start free
-  </a>
+        <div className="homeNavActions">
+          <Link className="homeNavLogin" href="/login">
+            Log in
+          </Link>
 
+          <Link className="homeNavCta" href="/signup">
+            Start free
+          </Link>
         </div>
+
       </div>
     </nav>
   );
